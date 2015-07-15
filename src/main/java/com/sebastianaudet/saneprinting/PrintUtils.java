7ecf9362140bef2a053c6994cfb2a756398e3376
Sebastian Audet <smaudet@my.trine.edu>
2015-07-15 12:22:37 -0400

    Sane printing defaults for Java

---
+++ src/main/java/com/sebastianaudet/saneprinting/PrintUtils.java
@@ -0,0 +1,14 @@
+package com.sebastianaudet.saneprinting;
+
+/**
+ * @author Sebastian Audet
+ * @version on 7/15/15.
+ */
+public class PrintUtils {
+  public static void pl(Object s){
+    System.out.print(s);
+  }
+  public static void ple(Object s){
+    System.err.print(s);
+  }
+}

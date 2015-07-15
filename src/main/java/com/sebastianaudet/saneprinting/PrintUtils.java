5af8a9583b2d49f9151590d3ef279b072994cad1
Sebastian Audet <smaudet@my.trine.edu>
2015-07-15 16:55:24 -0400

    Fix printline and add normal print functions

--- src/main/java/com/sebastianaudet/saneprinting/PrintUtils.java
+++ src/main/java/com/sebastianaudet/saneprinting/PrintUtils.java
@@ -6,9 +6,15 @@ package com.sebastianaudet.saneprinting;
  */
 public class PrintUtils {
   public static void pl(Object s){
-    System.out.print(s);
+    System.out.println(s);
   }
   public static void ple(Object s){
+    System.err.println(s);
+  }
+  public static void p(Object s){
+    System.out.print(s);
+  }
+  public static void pe(Object s){
     System.err.print(s);
   }
 }
